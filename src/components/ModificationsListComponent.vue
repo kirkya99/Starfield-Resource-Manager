057cf2defa777ae9d88a72382eae11c36b9033df
Fabian Schotte <47646726+kirkya99@users.noreply.github.com>
2024-06-14 10:55:53 +0200

    Update ModificationsListComponent.vue

--- src/components/ModificationsListComponent.vue
+++ src/components/ModificationsListComponent.vue
@@ -26,7 +26,7 @@ export default defineComponent({
             this.allModsNames = this.allModsList.map(item => item.Modification)
           })
           .catch((error) => console.error('Error fetching JSON data:', error))
-          .finally(console.log("JSON data fetched.")
+          .finally(console.log("JSON data fetched."))
     }
   }
 })
