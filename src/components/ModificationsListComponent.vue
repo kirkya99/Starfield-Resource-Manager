ea72afc7a320d033bdb60df9e17532214cfd8b2b
Fabian Schotte <47646726+kirkya99@users.noreply.github.com>
2024-06-15 00:14:20 +0200

    added working add item to list prototype for modifications component

--- src/components/ModificationsListComponent.vue
+++ src/components/ModificationsListComponent.vue
@@ -1,39 +1,60 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-// import { Modification } from 'src/data/Modification'
 import modificationsJson from '../data/modifications.json'
+import { Columns, Modification } from 'src/data/Modification'
 
 export default defineComponent({
   name: 'ModificationsListComponent',
 
   data () {
     return {
-      modifications: modificationsJson,
+      modifications: [] as Modification[],
+      savedModifications: [],
       selectedMod: null
     }
   },
   created () {
+    this.receiveModifications()
     console.log(this.modifications)
+    console.log(Columns)
   },
   methods: {
+    onSubmit () {
+      if (this.selectedMod != null) {
+        this.savedModifications.push(this.selectedMod)
+        this.selectedMod = null
+      }
+    },
+    validateModification (val: unknown): boolean | string {
+      return !!val || 'Please select a modification'
+    },
+    receiveModifications () {
+      this.modifications = modificationsJson.map(item => ({
+        Modification: item.Modification,
+        Type: item.Type,
+        Slot: item.Slot,
+        Resources: new Map<string, number>(Object.entries(item.Resources))
+      }))
+    }
   }
 })
 </script>
 
 <template>
-  <div class="row">
-    <div class="col-8">
-      <!-- TODO: Add Autocomplete -->
-<!--      <q-select v-model="selectedMod.Modification" :options="modifications" filled/>-->
+  <q-form @submit="onSubmit" @reset="() => selectedMod = null">
+    <div class="row justify-around items-center">
+      <div class="col-8">
+        <q-select filled v-model="selectedMod" :options="modifications" label="Modification" option-label="Modification" clearable use-input/>
+      </div>
+      <div class="col-3 ">
+        <q-btn align="around" type="submit" label="Save modification" color="primary" icon="add" size="lg" :rules="[validateModification]" />
+      </div>
     </div>
-    <div class="col-4">
-      <!-- TODO: Add Add Modification Button -->
-      <q-btn/>
-    </div>
-    <div class="col-all">
-      <!-- TODO: Add Tracked Modifications Table -->
-<!--      {{ selectedMod.Modification }}-->
-    </div>
+  </q-form>
+
+  <div class="col-all">
+    <!-- TODO: Add Tracked Modifications Table -->
+    {{ selectedMod }}
   </div>
 </template>
 
--- src/data/Modification.ts
+++ src/data/Modification.ts
@@ -1,13 +1,36 @@
 // src/types/Modification.ts
-export interface Resources {
-  [resource: string]: number;
-}
-
 export interface Modification {
   Modification: string;
   Type: string;
   Slot: string;
-  Resources: [ Resources ]
+  Resources: Map<string, number>
 }
+
+export const Columns = [
+  {
+    name: 'modification',
+    required: true,
+    label: 'Modification',
+    align: 'left',
+    field: 'Modification',
+    sortable: true
+  },
+  {
+    name: 'type',
+    required: true,
+    label: 'Type',
+    align: 'left',
+    field: 'Type',
+    sortable: true
+  },
+  {
+    name: 'slot',
+    required: true,
+    label: 'Slot',
+    align: 'left',
+    field: 'Slot',
+    sortable: true
+  }
+]
 
 // TODO: Add tracked modifications
