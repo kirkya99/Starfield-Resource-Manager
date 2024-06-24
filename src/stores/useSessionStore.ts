bc9c0afcc10cb58dbddf79aa49e84bff6d0f8a4f
Fabian Schotte <47646726+kirkya99@users.noreply.github.com>
2024-06-24 09:10:15 +0200

    Update useSessionStore.ts

--- src/stores/useSessionStore.ts
+++ src/stores/useSessionStore.ts
@@ -19,6 +19,7 @@ export const useSessionStore = defineStore('session', () => {
     modification.resources.forEach((value, key) => {
       const resource: CraftableResource | undefined = getCraftableResource(key)
       if (resource) {
+        console.log(`Manufactured: ${resource.name}`)
         addCraftableResource(resource)
       } else {
         nonCraftable.set(key, value)
@@ -40,7 +41,7 @@ export const useSessionStore = defineStore('session', () => {
   }
 
   const addCraftableResource = (craftable: CraftableResource) => {
-    console.log(craftable.name)
+    console.log(`Manufactured: ${craftable.name}`)
     craftableResources.value.push(craftable)
     const nonCraftable: Map<string, number> = new Map<string, number>()
 
